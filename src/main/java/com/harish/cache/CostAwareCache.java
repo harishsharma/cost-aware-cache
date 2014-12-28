be90ad7ccd867d8e991f9e735bfdcfb03d04551a
harish.sharma <harish.sharma@192.168.0.101>
2014-12-28 12:50:35 +0530

    Docs.

--- src/main/java/com/harish/cache/CostAwareCache.java
+++ src/main/java/com/harish/cache/CostAwareCache.java
@@ -3,10 +3,38 @@ package com.harish.cache;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * {@link CostAwareCache} is a cache implementation which takes cost of
+ * computing values into account. This cache can be used when cost of computing
+ * key-value is different for different KVs.
+ * <p>
+ * It combines LRU with cost of computing values to give a more novel eviction
+ * policy.
+ * <p>
+ * {@link CostAwareCache} is an implementation of Greedy dual size algorithm.
+ * <p>
+ * <i>Note: This implementation is thread unsafe and not optimized fully and
+ * just a draft implementation<i>
+ * 
+ * @see http://www.cse.iitb.ac.in/~gracias/webcaching/html/node31.html
+ * 
+ * @author harish.sharma
+ *
+ * @param <K>
+ *            key
+ * @param <V>
+ *            value
+ */
 public class CostAwareCache<K, V> {
 
 	private static final int MAXIMUM_CAPACITY = 1 << 30;
 
+	/*
+	 * Specialized implementation of binary heap which contains entries which
+	 * are aware of index on which corresponding entry is present. This is
+	 * required to make the get and put operation O(log(n)) at random element
+	 * without searching for the index of entry first.
+	 */
 	private final IndexAwareHeap<K, V> heap;
 	private final Map<K, IndexedEntry<K, V>> map;
 
@@ -30,14 +58,30 @@ public class CostAwareCache<K, V> {
 		map = new HashMap<K, IndexedEntry<K, V>>(capacity);
 	}
 
+	/**
+	 * returns the current size.
+	 * 
+	 * @return return the size of the cache.
+	 */
 	public int size() {
 		return size;
 	}
 
+	/**
+	 * 
+	 * @return is cache empty.
+	 */
 	public boolean isEmpty() {
 		return size == 0;
 	}
 
+	/**
+	 * This returns the value for this KV pair.
+	 * 
+	 * @param key
+	 * @return value associated with this key , or null in case this key is not
+	 *         present in the cache.
+	 */
 	public V get(K key) {
 		if (null == key)
 			return null;
@@ -51,6 +95,16 @@ public class CostAwareCache<K, V> {
 		return value;
 	}
 
+	/**
+	 * This method allows to put KV pairs and cost associated with this KV pair.
+	 * 
+	 * @param key
+	 *            key
+	 * @param value
+	 *            value associated with this key.
+	 * @param costOfValue
+	 *            cost of computing this value.
+	 */
 	public void put(K key, V value, int costOfValue) {
 		if (null == key)
 			throw new NullPointerException("Null keys are not supported");
@@ -85,6 +139,9 @@ public class CostAwareCache<K, V> {
 		size = heap.size;
 	}
 
+	/*
+	 * Calculates H value as per the Greedy dual size algorithm.
+	 */
 	private int calculateHValue(int cost) {
 		int result = everIncreasingLValue + cost;
 		IndexedEntry<K, V> holder = heap.peek();
